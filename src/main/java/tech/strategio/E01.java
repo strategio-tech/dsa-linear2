85d6380f0839409e6632cc6fe1fe1982da444a77
Deeps <100620410+deepti-tri@users.noreply.github.com>
2022-11-14 16:35:55 -0600

    :rocket: first commit

---
+++ src/main/java/tech/strategio/E01.java
@@ -0,0 +1,18 @@
+package tech.strategio;
+
+public class E01 {
+    /**
+     * Given two strings ransomNote and magazine,
+     * return true if ransomNote can be constructed by using the letters from magazine
+     * and false otherwise.
+     * See the README.md file for more information.
+     *
+     * @param ransomNote - string #1
+     * @param magazine - string #2
+     * @return
+     */
+    public boolean canConstruct(String ransomNote, String magazine) {
+        // @TODO Write the method
+        return false;
+    }
+}
---
+++ src/test/java/tech/strategio/E01Test.java
@@ -0,0 +1,35 @@
+package tech.strategio;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class E01Test {
+    @DisplayName("Ransom Note")
+    @ParameterizedTest(name = "{index} => {0} and {1} is {2}")
+    @MethodSource("arrayToBoolean")
+    void arrayToBoolean(String s, String t, Boolean result) {
+        E01 exercise = new E01();
+        boolean val = exercise.canConstruct(s,t);
+        assertEquals(result, val);
+    }
+    private static Stream<Arguments> arrayToBoolean() {
+        return Stream.of(
+                Arguments.arguments("aa", "aab", true),
+                Arguments.arguments("ab", "abb", true),
+                Arguments.arguments("a", "b", false),
+                Arguments.arguments("aa", "ab", true),
+                Arguments.arguments("x", "y", false),
+                Arguments.arguments("rat", "car", false),
+                Arguments.arguments("bok", "book", true),
+                Arguments.arguments("loot", "tool", true),
+                Arguments.arguments("lot", "tool", true)
+        );
+    }
+
+}
